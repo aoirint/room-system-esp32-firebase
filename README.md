f390d398b0f5351ae1697a2d02acb4ace45606a6
aoirint <aoirint@gmail.com>
2020-11-01 16:37:14 +0900

    teams notification

---
+++ README.md
@@ -0,0 +1,8 @@
+
+Flame/Blaze plan required for sending notifications.
+
+Set Microsoft Teams incoming webhook url.
+
+```
+firebase functions:config:set teams.incoming_webhook_url=https://outlook.office.com/webhook/...
+```
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,51 @@
 import * as functions from 'firebase-functions';
+import fetch from 'node-fetch';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+
+export const doorStateChanged = functions.database.ref('/sensor/door').onWrite((change, context) => {
+  if (!change.before.exists() || !change.after.exists()) return null;
+
+    const prevDoorIsOpen = change.before.child('isOpen').val();
+    const doorIsOpen = change.after.child('isOpen').val();
+
+    const sendMessageToTeams = (title: string, text: string) => {
+      const requestData = JSON.stringify({
+        'title': title,
+        'text': text,
+      });
+
+      return fetch(functions.config().teams.incoming_webhook_url, {
+        method: 'POST',
+        body: requestData,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    };
+
+    if (!prevDoorIsOpen && doorIsOpen) {
+      functions.logger.info("Door opened.", {structuredData: true});
+      return sendMessageToTeams('DoorState Notification', 'Door opened.').then((response) => {
+        functions.logger.info(`Door opening notified; Status: ${response.status}`, {structuredData: true});
+        functions.logger.info(response, {structuredData: true});
+      });
+    }
+
+    if (prevDoorIsOpen && !doorIsOpen) {
+      functions.logger.info("Door closed.", {structuredData: true});
+      return sendMessageToTeams('DoorState Notification', 'Door closed.').then((response) => {
+        functions.logger.info(`Door closing notified; Status: ${response.status}`, {structuredData: true});
+        functions.logger.info(response, {structuredData: true});
+      });
+    }
+
+    return null;
 });
+
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
